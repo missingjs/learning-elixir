bbed85166c5147eebdbdae7e71db444a78cccde4
Qiming Tang <missingjs@outlook.com>
2026-07-02 13:26:45 +0800

    Remove redundant code

--- projects/01-cli-todo/todo/lib/todo.ex
+++ projects/01-cli-todo/todo/lib/todo.ex
@@ -1,18 +1,2 @@
 defmodule Todo do
-  @moduledoc """
-  Documentation for `Todo`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Todo.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
