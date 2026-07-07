c7325b623fad43ac1bb2386402abe02263de456b
Qiming Tang <tangqiming@zhiman.cn>
2026-07-07 09:40:15 +0000

    Add json serialization helper function for Task module

--- projects/01-cli-todo/todo/lib/todo/task.ex
+++ projects/01-cli-todo/todo/lib/todo/task.ex
@@ -16,4 +16,22 @@ defmodule Todo.Task do
 
   @enforce_keys [:id, :title]
   defstruct [:id, :title, done: false]
+
+  @spec to_json(Todo.Task.t()) :: map()
+  def to_json(%__MODULE__{id: id, title: title, done: done} = _task) do
+    %{
+      "id" => id,
+      "title" => title,
+      "done" => done
+    }
+  end
+
+  @spec from_json(map()) :: t()
+  def from_json(%{"id" => id, "title" => title, "done" => done} = _json_map) do
+    %__MODULE__{
+      id: id,
+      title: title,
+      done: done
+    }
+  end
 end
---
+++ projects/01-cli-todo/todo/test/todo/task_test.exs
@@ -0,0 +1,36 @@
+defmodule Todo.TaskTest do
+  use ExUnit.Case, async: true
+  alias Todo.Task
+
+  describe "to_json/1" do
+    test "serialize to json map in happy path" do
+      task = %Task{
+        id: 1,
+        title: "First Task",
+        done: false
+      }
+
+      assert %{
+        "id" => 1,
+        "title" => "First Task",
+        "done" => false
+      } = Task.to_json(task)
+    end
+  end
+
+  describe "from_json/1" do
+    test "parse from json map in happy path" do
+      json_map = %{
+        "id" => 1,
+        "title" => "First Task",
+        "done" => false
+      }
+
+      assert %Task{
+        id: 1,
+        title: "First Task",
+        done: false
+      } = Task.from_json(json_map)
+    end
+  end
+end
